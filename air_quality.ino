90ecfeb831580424d6a4809e7c51b226bd29d911
Stephen Swanson <swanson.stephen.h@gmail.com>
2024-09-30 01:20:32 +0300

    sends data to sever to be fetched by air-control-frontend

--- air_quality.ino
+++ air_quality.ino
@@ -1,9 +1,16 @@
+#include <Arduino.h>
+#include <HTTPClient.h>
+#include <Wifi.h>
 #include <TFT_eSPI.h>
 #include <DHT.h>
 #include "SparkFun_SGP30_Arduino_Library.h"
-#include <Wire.h>
-#include "sf_pro.h"                // SF Pro font
+#include <Wire.h>           
 #include "gifs/lily_happy_crop.h"  // lily gif
+#include "sf_pro.h"
+
+
+const char* ssid = "( ͡° ͜ʖ ͡°)";
+const char* password = "OnePingToRuleThemAll";
 
 TFT_eSPI tft = TFT_eSPI();
 const int screenWidth = 120;
@@ -17,73 +24,41 @@ float humidity;
 float co2;
 float voc;
 
-// DHT11
-unsigned long lastUpdateTime = 0;  // Variable to store the last update time
-const unsigned long updateInterval = 5000;
+unsigned long lastUpdateTime = 0;
+const unsigned long updateInterval = 60000;
 
 String oldValues[4];
 
-void setup() {
-  Serial.begin(115200);
+void sendDataToServer(float temp, float humidity, float co2, float voc) {
+  if (WiFi.status() == WL_CONNECTED) { 
+    HTTPClient http; 
 
-  // Initialize TFT
-  Serial.println("Initializing TFT...");
-  tft.begin();
-  tft.setRotation(0);
-  tft.setSwapBytes(true);
-  tft.fillScreen(TFT_BLACK);
-  tft.loadFont(sfProFont);
-  Serial.println("TFT initialized.");
+    String serverPath = "https://air-control-25b7322984ae.herokuapp.com/sensor-data"; 
 
-  // Initialize DHT
-  Serial.println("Initializing DHT...");
-  dht.begin();
-  Serial.println("DHT initialized.");
+    String jsonPayload = "{\"temp\":" + String(temp) + 
+                     ",\"hum\":" + String(humidity) + 
+                     ",\"co2\":" + String(co2) + 
+                     ",\"voc\":" + String(voc) + 
+                     "}";
 
-  // Initialize I2C and SGP30
-  Serial.println("Initializing I2C...");
-  Wire.begin();
-  Serial.println("I2C initialized.");
-  
-  Serial.println("Initializing SGP30...");
-  if (!mySensor.begin()) {
-    Serial.println("No SGP30 Detected. Check connections.");
-    while (1);
+    http.begin(serverPath); 
+    http.addHeader("Content-Type", "application/json");
+
+    int httpResponseCode = http.POST(jsonPayload);
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+  } else {
+    Serial.println("WiFi Disconnected");
   }
-  mySensor.initAirQuality();
-  Serial.println("SGP30 initialized.");
-
-  // Allow sensor warm-up time
-  Serial.println("Allowing sensor warm-up time...");
-  delay(15000); // 15 seconds warm-up time
-  Serial.println("Warm-up complete.");
-}
-
-float calculateAbsoluteHumidity(float temp, float humidity) {
-  // Constants for the calculation
-  const float mw = 18.016;       // Molecular weight of water g/mol
-  const float r = 8.31447215;    // Universal gas constant J/(mol*K)
-
-  // Calculate absolute humidity
-  float absHumidity = (6.112 * exp((17.67 * temp) / (temp + 243.5)) * humidity * mw) / ((273.15 + temp) * r);
-  return absHumidity * 1000; // convert to g/m^3
-}
-
-void updateSensorReadings() {
-  // Read humidity and temperature
-  temp = dht.readTemperature();
-  humidity = dht.readHumidity();
-
-  // Set humidity for SGP30 sensor
-  if (!isnan(humidity) && !isnan(temp)) {
-    uint32_t absoluteHumidity = static_cast<uint32_t>(calculateAbsoluteHumidity(temp, humidity));
-    mySensor.setHumidity(absoluteHumidity);
-  }
-
-  // Read air quality
-  mySensor.measureAirQuality();
-  co2 = mySensor.CO2;
-  voc = mySensor.TVOC;
 }
 
 void displayInfoText() {
@@ -106,20 +81,66 @@ void displayInfoText() {
       int y = 240 - (numRows - row) * cellHeight + cellHeight / 2 - yOffset;
 
       if (values[index] != oldValues[index]) {
-        // Erase old text
         tft.setTextColor(TFT_BLACK, TFT_BLACK);
         tft.drawString(texts[index] + oldValues[index], x, y);
-
-        // Draw new text
         tft.setTextColor(TFT_WHITE, TFT_BLACK);
         tft.drawString(texts[index] + values[index], x, y);
-
-        // Update old value
         oldValues[index] = values[index];
       }
     }
   }
 }
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  tft.begin();
+  tft.setRotation(0);
+  tft.setSwapBytes(true);
+  tft.fillScreen(TFT_BLACK);
+  tft.loadFont(sfProFont);
+  dht.begin();
+  Wire.begin();
+  
+  Serial.println("Initializing SGP30...");
+  if (!mySensor.begin()) {
+    Serial.println("No SGP30 Detected. Check connections.");
+    while (1);
+  }
+  mySensor.initAirQuality();
+  Serial.println("SGP30 initialized.");
+  delay(15000);
+  displayInfoText();
+}
+
+float calculateAbsoluteHumidity(float temp, float humidity) {
+  const float mw = 18.016;       // Molecular weight of water g/mol
+  const float r = 8.31447215;    // Universal gas constant J/(mol*K)
+
+  float absHumidity = (6.112 * exp((17.67 * temp) / (temp + 243.5)) * humidity * mw) / ((273.15 + temp) * r);
+  return absHumidity * 1000;
+}
+
+void updateSensorReadings() {
+  temp = dht.readTemperature();
+  humidity = dht.readHumidity();
+
+  if (!isnan(humidity) && !isnan(temp)) {
+    uint32_t absoluteHumidity = static_cast<uint32_t>(calculateAbsoluteHumidity(temp, humidity));
+    mySensor.setHumidity(absoluteHumidity);
+  }
+
+  mySensor.measureAirQuality();
+  co2 = mySensor.CO2;
+  voc = mySensor.TVOC;
+}
+
 
 void loop() {
   unsigned long currentTime = millis();
@@ -127,11 +148,13 @@ void loop() {
   if (currentTime - lastUpdateTime >= updateInterval) {
     updateSensorReadings();
     displayInfoText();
+    sendDataToServer(temp, humidity, co2, voc);
+
     lastUpdateTime = currentTime;
   }
 
   for (int i = 0; i < frames; i++) {
-    tft.pushImage(screenWidth / 4 - 5, 0, lily_happy_cropWidth, lily_happy_cropHeight, lily_happy_crop[i]);
+    tft.pushImage(screenWidth / 2 - 13, 7, lily_happy_cropWidth, lily_happy_cropHeight, lily_happy_crop[i]);
     delay(200);
   }
 }
